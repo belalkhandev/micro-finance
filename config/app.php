3c349191e4245ca97beb4b6241a1ac2b94eaf454
belalkhandev <belalkhan.dev@gmail.com>
2022-03-25 19:20:24 +0600

    add mPDF

--- config/app.php
+++ config/app.php
@@ -179,7 +179,8 @@ return [
         App\Repositories\RepositoryServiceProvider::class,
 
         //intervention image
-        Intervention\Image\ImageServiceProvider::class
+        Intervention\Image\ImageServiceProvider::class,
+        niklasravnsborg\LaravelPdf\PdfServiceProvider::class
     ],
 
     /*
@@ -234,8 +235,8 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
-        'Image' => Intervention\Image\Facades\Image::class
-
+        'Image' => Intervention\Image\Facades\Image::class,
+        'PDF' => niklasravnsborg\LaravelPdf\Facades\Pdf::class
     ],
 
 ];
