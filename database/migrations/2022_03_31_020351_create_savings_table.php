d64fa1ee79ccede91da4a2d2ccdd08c985d8efe4
belalkhandev <belalkhan.dev@gmail.com>
2022-03-31 02:08:53 +0600

    Create savings table

---
+++ database/migrations/2022_03_31_020351_create_savings_table.php
@@ -0,0 +1,37 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateSavingsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('savings', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('member_id');
+            $table->unsignedBigInteger('loan_transaction_id')->nullable();
+            $table->enum('savings_type', ['deposit', 'withdraw'])->nullable();
+            $table->double('amount', 10, 2)->nullable();
+            $table->date('savings_date')->nullable();
+            $table->timestamps();
+            $table->foreign('member_id')->references('id')->on('members')->onDelete('cascade');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('savings');
+    }
+}
