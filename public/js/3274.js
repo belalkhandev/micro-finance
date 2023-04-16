4e2c8ea55c279006f3f1b91e33c714ff761f5b0e
Belal Khan <belalkhan.dev@gmail.com>
2023-04-16 23:51:53 +0600

    added prod

---
+++ public/js/3274.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[3274],{3274:(e,a,r)=>{r.r(a),r.d(a,{default:()=>u});var n=r(821);var s={class:"error-page 403"},c=[(0,n._)("img",{src:"/images/403.png?ab7bc51396944539dd0e311fe2ae8d88",alt:""},null,-1)];const t={name:"403"};const u=(0,r(3744).Z)(t,[["render",function(e,a,r,t,u,d){return(0,n.wg)(),(0,n.iD)("div",s,c)}]])}}]);
