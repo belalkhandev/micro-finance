dbf6fbda018bbe199dadcdfc87814150e1e860a3
Belal Khan <belalkhan.dev@gmail.com>
2023-04-17 00:47:59 +0600

    pdf font support issue solved

--- config/pdf.php
+++ config/pdf.php
@@ -1,19 +1,29 @@
 <?php
-
 return [
-	'mode'                  => 'utf-8',
-	'format'                => 'A4',
-	'author'                => '',
-	'subject'               => '',
-	'keywords'              => '',
-	'creator'               => 'Laravel Pdf',
-	'display_mode'          => 'fullpage',
-	'tempDir'               => base_path('../temp/'),
-	'pdf_a'                 => false,
-	'pdf_a_auto'            => false,
-	'icc_profile_path'      => '',
-    'font_path' => base_path('resources/fonts/'),
-    'font_data' => [
+    'mode'                       => '',
+    'format'                     => 'A4',
+    'default_font_size'          => '12',
+    'margin_left'                => 10,
+    'margin_right'               => 10,
+    'margin_top'                 => 10,
+    'margin_bottom'              => 10,
+    'margin_header'              => 0,
+    'margin_footer'              => 0,
+    'orientation'                => 'P',
+    'title'                      => 'Laravel mPDF',
+    'author'                     => '',
+    'watermark'                  => '',
+    'show_watermark'             => false,
+    'show_watermark_image'       => false,
+    'watermark_font'             => 'sans-serif',
+    'display_mode'               => 'fullpage',
+    'watermark_text_alpha'       => 0.1,
+    'watermark_image_path'       => '',
+    'watermark_image_alpha'      => 0.2,
+    'watermark_image_size'       => 'D',
+    'watermark_image_position'   => 'P',
+    'custom_font_dir'            => base_path('resources/fonts/'),
+    'custom_font_data'           => [
         'kalpurush' => [
             'R'  => 'kalpurush.ttf',    // regular font
             'B'  => 'kalpurush.ttf',       // optional: bold font
@@ -23,5 +33,10 @@ return [
             'useKashida' => 75,  // required for complicated langs like Persian, Arabic and Chinese
         ],
     ],
+    'auto_language_detection'    => false,
+    'temp_dir'                   => storage_path('app'),
+    'pdfa'                       => false,
+    'pdfaauto'                   => false,
+    'use_active_forms'           => false,
     'default_font' => 'Roboto'
 ];
